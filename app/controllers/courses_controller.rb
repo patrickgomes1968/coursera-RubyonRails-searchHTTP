e3db0c85b913ae95c36490d0d42b23823f9f0da4
Patrick Gomes <patrickgomes@hotmail.com>
2016-02-18 22:34:58 +0000

    changed search string from jhu to diet

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   def index
-  	@search_term = params[:looking_for] || 'jhu'
+  	@search_term = params[:looking_for] || 'diet'
   	@courses = Coursera.for(@search_term)
   end
 end
